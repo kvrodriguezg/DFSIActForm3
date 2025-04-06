a724f8230c2748a112b1058af004dc6ee7abac84
Katlheen Rodriguez <kv.rodriguezgarcia@gmail.com>
2025-04-06 17:19:53 -0400

    Primer commit

---
+++ src/main/java/com/exp2_s4_katlheen_rodriguez/exp2_s4_katlheen_rodriguez/Exp2S4KatlheenRodriguezApplication.java
@@ -0,0 +1,13 @@
+package com.exp2_s4_katlheen_rodriguez.exp2_s4_katlheen_rodriguez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp2S4KatlheenRodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp2S4KatlheenRodriguezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exp2_s4_katlheen_rodriguez/exp2_s4_katlheen_rodriguez/Exp2S4KatlheenRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp2_s4_katlheen_rodriguez.exp2_s4_katlheen_rodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp2S4KatlheenRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
